34787bf0c52e744b457f4dea01ca50c21545f573
dgonzalez7 <gonzalez@thosegonzos.com>
2014-05-17 16:26:46 -0400

    Fix spacing

--- src/EcplispeTest.java
+++ src/EcplispeTest.java
@@ -3,7 +3,7 @@ public class EcplispeTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("\nEclipse & Git");
+		System.out.println("Eclipse & Git");
 
 	}
 
